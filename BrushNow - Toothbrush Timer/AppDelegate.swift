38bc96274de5cd5dfb30a21d2dffe0c80bff7718
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-09-29 16:05:17 +0800

    Correct time settings now show up when launched (I think)

--- BrushNow - Toothbrush Timer/AppDelegate.swift
+++ BrushNow - Toothbrush Timer/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UserDefaults.standard.register(defaults: ["timeSet": 120])
+        let loadedTimeSet = UserDefaults.standard.integer(forKey: "timeSet")
+        timeSet = loadedTimeSet
         return true
     }
 
@@ -39,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        UserDefaults.standard.set(timeSet, forKey: "timeSet")
     }
 
 
