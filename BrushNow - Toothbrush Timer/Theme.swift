6c83fdf9d77739e3c6ecb885833c9e6300cd83fc
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-10-06 10:54:51 +0800

    Themes saving (I think?)

--- BrushNow - Toothbrush Timer/Theme.swift
+++ BrushNow - Toothbrush Timer/Theme.swift
@@ -10,10 +10,47 @@ import Foundation
 import UIKit
 
 
-struct Theme {
+class Theme: NSObject, NSCoding {
     var name: String
     var textColour: UIColor
     var backgroundColour: UIColor
     var buttonColour: UIColor
     var previewImage: String
+    
+    init (name: String, textColour: UIColor, backgroundColour: UIColor, buttonColour: UIColor, previewImage: String) {
+        self.name = name
+        self.textColour = textColour
+        self.backgroundColour = backgroundColour
+        self.buttonColour = buttonColour
+        self.previewImage = previewImage
+    }
+    
+    // NSCODING
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.name, forKey: "name")
+        aCoder.encode(self.textColour, forKey: "textColour")
+        aCoder.encode(self.backgroundColour, forKey: "backgroundColour")
+        aCoder.encode(self.buttonColour, forKey: "buttonColour")
+        aCoder.encode(self.previewImage, forKey: "previewImage")
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard
+            let name = aDecoder.decodeObject(forKey: "name") as? String,
+            let textColour = aDecoder.decodeObject(forKey: "textColour") as? UIColor,
+            let backgroundColour = aDecoder.decodeObject(forKey: "backgroundColour") as? UIColor,
+            let buttonColour = aDecoder.decodeObject(forKey: "buttonColour") as? UIColor,
+            let previewImage = aDecoder.decodeObject(forKey: "previewImage") as? String
+            else { return nil }
+        
+        self.init(
+            name: name,
+            textColour: textColour,
+            backgroundColour: backgroundColour,
+            buttonColour: buttonColour,
+            previewImage: previewImage
+        )
+    }
+    
 }
