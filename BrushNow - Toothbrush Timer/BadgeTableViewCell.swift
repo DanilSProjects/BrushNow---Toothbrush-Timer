81a512176f4daae42e3817afd19ac32e92173b83
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-10-15 14:28:53 +0800

    Layout fix (it glitched out sometimes before)

--- BrushNow - Toothbrush Timer/BadgeTableViewCell.swift
+++ BrushNow - Toothbrush Timer/BadgeTableViewCell.swift
@@ -21,11 +21,6 @@ class BadgeTableViewCell: UITableViewCell {
         // Initialization code
     }
     
-    // Making description label align to top left
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        descLabel.sizeToFit()
-    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
