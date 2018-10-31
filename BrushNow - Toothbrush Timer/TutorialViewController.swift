6e36191b2cd05d8014117ff87981f53fd0ba4bc7
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-10-31 21:59:38 +0800

    Tutorial implemented

--- BrushNow - Toothbrush Timer/TutorialViewController.swift
+++ BrushNow - Toothbrush Timer/TutorialViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 
 class TutorialViewController: UIViewController {
     
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTwo: UILabel!
+    @IBOutlet weak var contButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelOne.alpha = 0
+        labelTwo.alpha = 0
+        contButton.alpha = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let animator = UIViewPropertyAnimator(duration: 1, curve: .linear, animations: {
+            self.labelOne.alpha = 1
+        })
+        animator.addCompletion{(_) in
+            let secAnimator = UIViewPropertyAnimator(duration: 1, curve: .linear, animations: {
+                self.labelTwo.alpha = 1
+            })
+            secAnimator.addCompletion{ (_) in
+                let thirAnimator = UIViewPropertyAnimator(duration: 1, curve: .linear, animations: {
+                    self.contButton.alpha = 1
+                })
+                
+                thirAnimator.startAnimation()
+            }
+            secAnimator.startAnimation()
+        }
+        animator.startAnimation()
     }
     
     
